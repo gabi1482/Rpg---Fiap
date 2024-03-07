4729de46fc47bbe830b5ab7a83d80c50e7541685
ADM <ADM@DESKTOP-BVUKACK>
2024-03-07 11:58:22 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rpg___Fiap.Models;
+
+namespace Rpg___Fiap.Controllers
+{
+
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class CharacterController : ControllerBase
+    {
+       
+        public static Character Kinight = new Character(); 
+
+        [HttpGet]
+        public ActionResult<Character> get()
+        {
+            return Ok(Kinight);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+global using Rpg___Fiap.Models; //qualquer arquivo que for criado nesse proeto pode ter acesso a pasta models. Ou seja, todos os arquivos que eu criar na pasta models, eu nao preciso ficar importando
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
